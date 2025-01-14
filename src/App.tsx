3c21912e5d69eeab2d7323ef43b2c748f247c358
ruth7873 <ruth0533137873@gmail.com>
2025-01-14 20:25:58 +0200

    change

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,15 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Home from './pages/Home';
+// import Home from './pages/Home';
 import Portfolio from './pages/Portfolio';
-import Contact from './pages/Contact';
+// import Contact from './pages/Contact';
 import './styles/global.css';
 
 const App: React.FC = () => {
   return (
     <Router>
       <Routes>
-
-
-        <Route path="/"  Component={Home} />
-        <Route path="/portfolio" Component={Portfolio} />
-        <Route path="/contact" Component={Contact} />
+        <Route path="/"  Component={Portfolio} />
       </Routes>
     </Router>
   );
